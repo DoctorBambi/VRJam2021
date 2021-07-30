9951ff551eca5d3c70e20bafb83cda1d0d4ee328
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-07-30 18:30:04 -0400

    Added chunk spawning

--- Assets/Noureks Coding Stuff/ChunkLoading.cs
+++ Assets/Noureks Coding Stuff/ChunkLoading.cs
@@ -4,27 +4,50 @@ using UnityEngine;
 
 public class ChunkLoading : MonoBehaviour
 {
+	AsteroidSpawner astSpawner;
 	[SerializeField]
+	//curently occupied chunk
 	Vector3Int chunk; 
 	[SerializeField]
 	Transform player;
 	public float chunkScale;
 	chunk[,,] chunks;
+	[SerializeField]
+	int numAsteroidsPerChunk = 10;
+	void Start(){
+		chunks = new chunk[20,20,20];
+		astSpawner = GetComponent<AsteroidSpawner>();
+	}
 	Vector3Int GetCurrentChunk(){
 		Vector3 chunk_rough=player.position/chunkScale;
 		return new Vector3Int(Mathf.RoundToInt(chunk_rough.x),Mathf.RoundToInt(chunk_rough.y),Mathf.RoundToInt(chunk_rough.z));
 	}
 	void Update(){
-		bool changedChunkThisFrame = GetCurrentChunk() == chunk;
+		bool changedChunkThisFrame = GetCurrentChunk() != chunk;
 		chunk = GetCurrentChunk();
+		/* Debug.Log(GetCurrentChunk()); */
+		/* Debug.Log(chunk); */
+		chunk chunkVar = chunks[GetCurrentChunk().x,GetCurrentChunk().y,GetCurrentChunk().z];
 		if(changedChunkThisFrame){
-			if(chunks[chunk.x,chunk.y,chunk.z] != null){
-				Debug.Log("Entered new chunk");
+			if(chunkVar != null){
+				foreach (Vector3 curCoord in chunkVar.asteroids)
+				{
+					astSpawner.SpawnAsteroid(curCoord);	
+				}	
+			}else{
+				for (int i = 0; i < numAsteroidsPerChunk; i++)
+				{
+					astSpawner.SpawnAsteroid(player.position);
+				}
+			}
+			foreach (Vector3 coord in chunkVar.asteroids)
+			{
+				Debug.Log(Physics.OverlapSphere(coord, .2f)[0].gameObject);	
 			}
 		}
 	}
 
 }
 public class chunk{
-	Vector3[] asteroids;
+	public Vector3[] asteroids;
 }
