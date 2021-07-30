02051b6fa65d84a77a2af4b837b2b446ffcd37b4
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-07-30 19:00:47 -0400

    Added chunk spawning fully

---
+++ Assets/Noureks Coding Stuff/AsteroidDespawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidDespawner : MonoBehaviour
+{
+	ChunkLoading cl;
+	float dist = 5;
+	void Start(){
+		cl = FindObjectOfType<ChunkLoading>();
+	}
+	void Update(){
+		if(transform.position.x>(float)cl.chunk.x+dist||transform.position.x<(float)cl.chunk.x-dist||transform.position.x>(float)cl.chunk.y+dist||transform.position.x<(float)cl.chunk.y-dist||transform.position.x>(float)cl.chunk.z+dist||transform.position.x<(float)cl.chunk.z-dist){
+			Destroy(this.gameObject);
+		}
+	}
+}
