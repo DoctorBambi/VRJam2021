36ce8395eeeaf90e9e0bb9b32dccfc42092c1a9a
DirtyBong <76145567+DirtyBong@users.noreply.github.com>
2021-07-31 15:33:46 -0400

    Added A Locomotion Helper

---
+++ Assets/BallCollisionHelp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollisionHelp : MonoBehaviour
+{
+    [SerializeField] private Transform jetHand0;
+    [SerializeField] private Transform jetHand1;
+
+    private void Start()
+    {
+        Physics.IgnoreCollision(jetHand0.GetComponent<Collider>(), GetComponent<Collider>());
+        Physics.IgnoreCollision(jetHand1.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+}
