53ba9ceb6147a8a038ad9b60e56075a5dd1e6eb8
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-08-01 13:17:44 -0400

    Added pause menu

---
+++ Assets/Noureks Coding Stuff/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+	bool toggle = false;
+	bool menuButton;
+	[SerializeField]
+	GameObject UI;
+    void Update()
+    {
+        menuButton = InputBridge.Instance.AButtonDown;
+		if(menuButton){
+			toggle = !toggle;
+			UI.SetActive(toggle);
+		}
+    }
+	public void loadMenu(){
+		SceneManager.LoadScene(0);	
+	}
+}
