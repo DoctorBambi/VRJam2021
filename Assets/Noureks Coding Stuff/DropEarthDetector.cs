02fcd1e8e4d89c1ea1c0fdb8789103e47e90bf55
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-08-01 18:22:15 -0400

    Fixed bugs"
    "

--- Assets/Noureks Coding Stuff/DropEarthDetector.cs
+++ Assets/Noureks Coding Stuff/DropEarthDetector.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DropEarthDetector : MonoBehaviour
 {
 	public Transform player;
 	public float maxDistance = 20;
 	void Update(){
 		if(Vector3.Distance(transform.position, player.position)>maxDistance){
-			FindObjectOfType<scriptEarth>().HandleDamage(Mathf.Infinity);
+			SceneManager.LoadScene(2);
 		}
 	}
 }
