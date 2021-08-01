92de1ec3bf7f095b5778cee1d8fbcd9ed48da284
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-08-01 14:42:30 -0400

    Added main menu

---
+++ Assets/Noureks Coding Stuff/MenuCode.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuCode : MonoBehaviour
+{
+	public void LoadScene(int index){
+		SceneManager.LoadScene(index);
+	}
+}
